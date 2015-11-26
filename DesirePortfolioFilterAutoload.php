813bca6c66a7737aa42221671ec2333c81dc84f3
desirelabs <desirelabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-26 09:39:53 +0000

    code reformat
    git-svn-id: http://plugins.svn.wordpress.org/desire-portfolio-filter/trunk@1294867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DesirePortfolioFilterAutoload.php
+++ DesirePortfolioFilterAutoload.php
@@ -5,14 +5,13 @@
  * @author franck LEBAS
  * @package desire-portfolio-filter
  */
-spl_autoload_register(function($class){
-    $classFile = dirname(__FILE__) . '/src/' .$class . '.php';
+spl_autoload_register(function ($class) {
+    $classFile = dirname(__FILE__) . '/src/' . $class . '.php';
     try {
         // Check if class file exists
         if (file_exists($classFile))
             include 'src/' . $class . '.php';
-    }
-    catch (Exception $e) {
+    } catch (Exception $e) {
         echo 'Classes not loaded : ' . $e->getMessage();
     }
 });
