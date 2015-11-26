e63e272ba8dd4d8fc54355edd90c5d850f214caf
FLE <f.lebas@nouvellemarque.com>
2015-11-26 10:31:55 +0100

    language fix

--- DesirePortfolioFilter.php
+++ DesirePortfolioFilter.php
@@ -11,42 +11,20 @@
  * Domaine Path: /lang
  */
 
-define( 'DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR', plugin_dir_url( __FILE__ ) );
-
-/**
- * First check if jetpack is activated
- * Else, Desire Portfolio Filter cannot be activated
- */
-function is_jetpack() {
-	if ( is_admin() && current_user_can( 'activate_plugins' ) && ! is_plugin_active( 'jetpack/jetpack.php' ) ) {
-		add_action( 'admin_notices', 'disabled_notice' );
-
-		deactivate_plugins( plugin_basename( __FILE__ ) );
-
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
-	}
-}
-
-add_action( 'admin_init', 'is_jetpack' );
-
-/**
- * Message shown if Jetpack not activated
- */
-function disabled_notice() {
-	$class   = "error";
-	$message = print( __( 'Desire Porfolio Filter requires Jetpack plugin and Custom Post Types Portfolio feature to be activated', 'desire-portfolio-filter' ) );
-	echo "<div class=\"$class\"> <p>$message</p></div>";
-}
-
+define( 'DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR', __DIR__ );
+define( 'DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL', plugin_dir_url(__FILE__) );
 require_once 'DesirePortfolioFilterAutoload.php';
 
 class DesirePortfolioFilter {
 
-	static function init() {
-		// Language
-		load_theme_textdomain( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . 'lang' );
+    /**
+     * Init hooks
+     */
+    static function init() {
+
+        // Theme setup
+        add_action( 'admin_init', array( __CLASS__, 'is_jetpack' ) );
+        add_action('after_setup_theme', array( __CLASS__, 'desire_theme_setup' ) );
 
 		// Hooks and actions init
 		add_theme_support( 'jetpack-portfolio' );
@@ -57,17 +35,42 @@ class DesirePortfolioFilter {
 	}
 
 
+    static function desire_theme_setup(){
+        load_theme_textdomain('desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . '/lang');
+    }
+
+
 	static function desire_portfolio_scripts() {
 
-		wp_register_script( 'desire-isotope', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . 'js/isotope.pkgd.min.js', array( 'jquery' ), '2.2.2', true );
-		wp_register_script( 'desire-images-loaded', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . 'js/imagesloaded.pkgd.min.js', array( 'jquery', 'desire-isotope' ), '3.2.0', true );
-		wp_register_style( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . 'css/desire-portfolio-filter.css', array(), '0.2', 'all' );
-		wp_register_script( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . 'js/desire-portfolio-filter.js', array( 'jquery', 'desire-isotope', 'desire-images-loaded' ), '0.2', true );
+		wp_register_script( 'desire-isotope', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/isotope.pkgd.min.js', array( 'jquery' ), '2.2.2', true );
+		wp_register_script( 'desire-images-loaded', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/imagesloaded.pkgd.min.js', array( 'jquery', 'desire-isotope' ), '3.2.0', true );
+		wp_register_style( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'css/desire-portfolio-filter.css', array(), '0.2', 'all' );
+		wp_register_script( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/desire-portfolio-filter.js', array( 'jquery', 'desire-isotope', 'desire-images-loaded' ), '0.2', true );
 
 		wp_enqueue_style( 'desire-portfolio-filter' );
 		wp_enqueue_script( 'desire-isotope' );
 		wp_enqueue_script( 'desire-images-loaded' );
 		wp_enqueue_script( 'desire-portfolio-filter' );
+	}
+
+
+	static function is_jetpack() {
+		if ( is_admin() && current_user_can( 'activate_plugins' ) && ! is_plugin_active( 'jetpack/jetpack.php' ) ) {
+			add_action( 'admin_notices', array( __CLASS__, 'disabled_notice' ) );
+
+			deactivate_plugins( plugin_basename( __FILE__ ) );
+
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+		}
+	}
+
+
+	static function disabled_notice() {
+		$class   = "error";
+		$message = __( 'Desire Porfolio Filter requires Jetpack plugin and Custom Post Types Portfolio feature to be activated', 'desire-portfolio-filter' );
+		echo "<div class=\"$class\"><p>$message</p></div>";
 	}
 }
 
