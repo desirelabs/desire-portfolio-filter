e0b4553c856129ef0e7216c5cdb6e95020c19789
desirelabs <desirelabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-26 09:39:53 +0000

    code reformat
    git-svn-id: http://plugins.svn.wordpress.org/desire-portfolio-filter/trunk@1294867 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DesirePortfolioFilter.php
+++ DesirePortfolioFilter.php
@@ -11,67 +11,73 @@
  * Domaine Path: /lang
  */
 
-define( 'DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR', __DIR__ );
-define( 'DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL', plugin_dir_url(__FILE__) );
+define('DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR', __DIR__);
+define('DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
 require_once 'DesirePortfolioFilterAutoload.php';
 
-class DesirePortfolioFilter {
+class DesirePortfolioFilter
+{
 
     /**
      * Init hooks
      */
-    static function init() {
+    static function init()
+    {
 
         // Theme setup
-        add_action( 'admin_init', array( __CLASS__, 'is_jetpack' ) );
-        add_action('after_setup_theme', array( __CLASS__, 'desire_theme_setup' ) );
+        add_action('admin_init', array(__CLASS__, 'is_jetpack'));
+        add_action('after_setup_theme', array(__CLASS__, 'desire_theme_setup'));
 
-		// Hooks and actions init
-		add_theme_support( 'jetpack-portfolio' );
-		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'desire_portfolio_scripts' ) );
+        // Hooks and actions init
+        add_theme_support('jetpack-portfolio');
+        add_action('wp_enqueue_scripts', array(__CLASS__, 'desire_portfolio_scripts'));
 
-		// Loads portfolio template
-		add_action( 'plugins_loaded', array( 'DesirePortfolioTemplate', 'get_instance' ) );
-	}
+        // Loads portfolio template
+        add_action('plugins_loaded', array('DesirePortfolioTemplate', 'get_instance'));
+    }
 
 
-    static function desire_theme_setup(){
+    static function desire_theme_setup()
+    {
         load_theme_textdomain('desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR . '/lang');
     }
 
 
-	static function desire_portfolio_scripts() {
+    static function desire_portfolio_scripts()
+    {
 
-		wp_register_script( 'desire-isotope', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/isotope.pkgd.min.js', array( 'jquery' ), '2.2.2', true );
-		wp_register_script( 'desire-images-loaded', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/imagesloaded.pkgd.min.js', array( 'jquery', 'desire-isotope' ), '3.2.0', true );
-		wp_register_style( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'css/desire-portfolio-filter.css', array(), '0.2', 'all' );
-		wp_register_script( 'desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/desire-portfolio-filter.js', array( 'jquery', 'desire-isotope', 'desire-images-loaded' ), '0.2', true );
+        wp_register_script('desire-isotope', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/isotope.pkgd.min.js', array('jquery'), '2.2.2', true);
+        wp_register_script('desire-images-loaded', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/imagesloaded.pkgd.min.js', array('jquery', 'desire-isotope'), '3.2.0', true);
+        wp_register_style('desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'css/desire-portfolio-filter.css', array(), '0.2', 'all');
+        wp_register_script('desire-portfolio-filter', DESIRE_PORTFOLIO_FILTER_PLUGIN_DIR_URL . 'js/desire-portfolio-filter.js', array('jquery', 'desire-isotope', 'desire-images-loaded'), '0.2', true);
 
-		wp_enqueue_style( 'desire-portfolio-filter' );
-		wp_enqueue_script( 'desire-isotope' );
-		wp_enqueue_script( 'desire-images-loaded' );
-		wp_enqueue_script( 'desire-portfolio-filter' );
-	}
-
-
-	static function is_jetpack() {
-		if ( is_admin() && current_user_can( 'activate_plugins' ) && ! is_plugin_active( 'jetpack/jetpack.php' ) ) {
-			add_action( 'admin_notices', array( __CLASS__, 'disabled_notice' ) );
-
-			deactivate_plugins( plugin_basename( __FILE__ ) );
-
-			if ( isset( $_GET['activate'] ) ) {
-				unset( $_GET['activate'] );
-			}
-		}
-	}
+        wp_enqueue_style('desire-portfolio-filter');
+        wp_enqueue_script('desire-isotope');
+        wp_enqueue_script('desire-images-loaded');
+        wp_enqueue_script('desire-portfolio-filter');
+    }
 
 
-	static function disabled_notice() {
-		$class   = "error";
-		$message = __( 'Desire Porfolio Filter requires Jetpack plugin and Custom Post Types Portfolio feature to be activated', 'desire-portfolio-filter' );
-		echo "<div class=\"$class\"><p>$message</p></div>";
-	}
+    static function is_jetpack()
+    {
+        if (is_admin() && current_user_can('activate_plugins') && !is_plugin_active('jetpack/jetpack.php')) {
+            add_action('admin_notices', array(__CLASS__, 'disabled_notice'));
+
+            deactivate_plugins(plugin_basename(__FILE__));
+
+            if (isset($_GET['activate'])) {
+                unset($_GET['activate']);
+            }
+        }
+    }
+
+
+    static function disabled_notice()
+    {
+        $class = "error";
+        $message = __('Desire Porfolio Filter requires Jetpack plugin and Custom Post Types Portfolio feature to be activated', 'desire-portfolio-filter');
+        echo "<div class=\"$class\"><p>$message</p></div>";
+    }
 }
 
 DesirePortfolioFilter::init();
--- src/DesirePortfolioTemplate.php
+++ src/DesirePortfolioTemplate.php
@@ -6,108 +6,113 @@
  * @Contributor: Franck LEBAS
  * @package desire-portfolio-filter
  */
-class DesirePortfolioTemplate {
+class DesirePortfolioTemplate
+{
 
-	/**
-	 * A reference to an instance of this class.
-	 */
-	private static $instance;
+    /**
+     * A reference to an instance of this class.
+     */
+    private static $instance;
 
-	/**
-	 * The array of templates that this plugin tracks.
-	 */
-	protected $templates;
+    /**
+     * The array of templates that this plugin tracks.
+     */
+    protected $templates;
 
-	/**
-	 * Returns an instance of this class.
-	 */
-	public static function get_instance() {
+    /**
+     * Returns an instance of this class.
+     */
+    public static function get_instance()
+    {
 
-		if ( null == self::$instance ) {
-			self::$instance = new DesirePortfolioTemplate();
-		}
+        if (null == self::$instance) {
+            self::$instance = new DesirePortfolioTemplate();
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 
-	/**
-	 * Initializes the class by setting filters and page functions.
-	 */
-	private function __construct() {
+    /**
+     * Initializes the class by setting filters and page functions.
+     */
+    private function __construct()
+    {
 
-		$this->templates = array();
+        $this->templates = array();
 
-		// Add a filter to the attributes metabox to inject template into the cache.
-		add_filter( 'page_attributes_dropdown_pages_args', array( $this, 'register_project_templates' ) );
+        // Add a filter to the attributes metabox to inject template into the cache.
+        add_filter('page_attributes_dropdown_pages_args', array($this, 'register_project_templates'));
 
-		// Add a filter to the save post to inject out template into the page cache
-		add_filter( 'wp_insert_post_data', array( $this, 'register_project_templates' ) );
+        // Add a filter to the save post to inject out template into the page cache
+        add_filter('wp_insert_post_data', array($this, 'register_project_templates'));
 
-		// Add a filter to the template include to determine if the page has our
-		// template assigned and return it's path
-		add_filter( 'template_include', array( $this, 'view_project_template' ) );
+        // Add a filter to the template include to determine if the page has our
+        // template assigned and return it's path
+        add_filter('template_include', array($this, 'view_project_template'));
 
-		// Template to use is set here
-		$this->templates = array(
-			'templates/portfolio-template.php' => 'Portfolio Template'
-		);
-	}
+        // Template to use is set here
+        $this->templates = array(
+            'templates/portfolio-template.php' => 'Portfolio Template'
+        );
+    }
 
 
-	/**
-	 * Adds the template to the pages cache in order to trick WordPress
-	 * into thinking the template file exists where it doens't really exist.
-	 */
-	public function register_project_templates( $atts ) {
+    /**
+     * Adds the template to the pages cache in order to trick WordPress
+     * into thinking the template file exists where it doens't really exist.
+     */
+    public function register_project_templates($atts)
+    {
 
-		// Create the key used for the themes cache
-		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
+        // Create the key used for the themes cache
+        $cache_key = 'page_templates-' . md5(get_theme_root() . '/' . get_stylesheet());
 
-		// Retrieve the cache list.
-		// If it doesn't exist, or it's empty prepare an array
-		$templates = wp_get_theme()->get_page_templates();
-		if ( empty( $templates ) ) {
-			$templates = array();
-		}
+        // Retrieve the cache list.
+        // If it doesn't exist, or it's empty prepare an array
+        $templates = wp_get_theme()->get_page_templates();
+        if (empty($templates)) {
+            $templates = array();
+        }
 
-		// New cache, therefore remove the old one
-		wp_cache_delete( $cache_key, 'themes' );
+        // New cache, therefore remove the old one
+        wp_cache_delete($cache_key, 'themes');
 
-		// Now add our template to the list of templates by merging our templates
-		// with the existing templates array from the cache.
-		$templates = array_merge( $templates, $this->templates );
+        // Now add our template to the list of templates by merging our templates
+        // with the existing templates array from the cache.
+        $templates = array_merge($templates, $this->templates);
 
-		// Add the modified cache to allow WordPress to pick it up for listing
-		// available templates
-		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
+        // Add the modified cache to allow WordPress to pick it up for listing
+        // available templates
+        wp_cache_add($cache_key, $templates, 'themes', 1800);
 
-		return $atts;
-	}
+        return $atts;
+    }
 
-	/**
-	 * Checks if the template is assigned to the page
-	 */
-	public function view_project_template( $template ) {
+    /**
+     * Checks if the template is assigned to the page
+     */
+    public function view_project_template($template)
+    {
 
-		global $post;
+        global $post;
 
-		if ( ! isset( $this->templates[ get_post_meta( $post->ID, '_wp_page_template', true ) ] ) ) {
-			return $template;
-		}
+        if (!isset($this->templates[get_post_meta($post->ID, '_wp_page_template', true)])) {
+            return $template;
+        }
 
-		$file = plugin_dir_path( __FILE__ ) . get_post_meta(
-			$post->ID, '_wp_page_template', true
-		);
+        $file = plugin_dir_path(__FILE__) . get_post_meta(
+                $post->ID, '_wp_page_template', true
+            );
 
-		// Just to be safe, we check if the file exist first
-		if ( file_exists( $file ) ) {
-			return $file;
-		} else {
-			echo $file;
-		}
+        // Just to be safe, we check if the file exist first
+        if (file_exists($file)) {
+            return $file;
+        } else {
+            echo $file;
+        }
 
-		return $template;
-	}
+        return $template;
+    }
 }
 
 ?>
